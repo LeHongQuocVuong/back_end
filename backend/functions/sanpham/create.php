be2a80fb31dae582c567e25dc7db920ee078e156
vuongd20084 <lhqvuongd20084@cusc.ctu.edu.vn>
2021-01-06 19:25:30 +0700

    create, edit: chưa validate

--- backend/functions/sanpham/create.php
+++ backend/functions/sanpham/create.php
@@ -25,133 +25,214 @@
           <h1 class="h2">Tạo sản phẩm</h1>
         </div>
 
+        <?php
+        // Truy vấn database
+        // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+        include_once(__DIR__ . '/../../../dbconnect.php');
+
+        /* --- 
+        --- 2.Truy vấn dữ liệu Loại sản phẩm 
+        --- 
+        */
+        // Chuẩn bị câu truy vấn Loại sản phẩm
+        $sqlLoaiSanPham = "select * from loaisanpham";
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultLoaiSanPham = mysqli_query($conn, $sqlLoaiSanPham);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataLoaiSanPham = [];
+        while ($rowLoaiSanPham = mysqli_fetch_array($resultLoaiSanPham, MYSQLI_ASSOC)) {
+            $dataLoaiSanPham[] = array(
+                'lsp_ma' => $rowLoaiSanPham['lsp_ma'],
+                'lsp_ten' => $rowLoaiSanPham['lsp_ten'],
+                'lsp_mota' => $rowLoaiSanPham['lsp_mota'],
+            );
+        }
+        /* --- End Truy vấn dữ liệu Loại sản phẩm --- */
+        
+        /* --- 
+        --- 3. Truy vấn dữ liệu Nhà sản xuất 
+        --- 
+        */
+        // Chuẩn bị câu truy vấn Nhà sản xuất
+        $sqlNhaSanXuat = "select * from nhasanxuat";
+
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultNhaSanXuat = mysqli_query($conn, $sqlNhaSanXuat);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataNhaSanXuat = [];
+        while ($rowNhaSanXuat = mysqli_fetch_array($resultNhaSanXuat, MYSQLI_ASSOC)) {
+            $dataNhaSanXuat[] = array(
+                'nsx_ma' => $rowNhaSanXuat['nsx_ma'],
+                'nsx_ten' => $rowNhaSanXuat['nsx_ten'],
+            );
+        }
+        /* --- End Truy vấn dữ liệu Nhà sản xuất --- */
+
+        // Chuẩn bị câu truy vấn Khuyến mãi
+        $sqlKhuyenMai = "select * from `khuyenmai`";
+
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultKhuyenMai = mysqli_query($conn, $sqlKhuyenMai);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataKhuyenMai = [];
+        while($rowKhuyenMai = mysqli_fetch_array($resultKhuyenMai, MYSQLI_ASSOC)){
+          $km_tomtat = '';
+          if(!empty($rowKhuyenMai['km_ten'])){
+            // Sử dụng hàm sprintf() để chuẩn bị mẫu câu với các giá trị truyền vào tương ứng từng vị trí placeholder
+            $km_tomtat = sprintf(
+                "Khuyến mãi %s, nội dung: %s, thời gian: %s-%s",
+                $rowKhuyenMai['km_ten'],
+                $rowKhuyenMai['kh_noidung'],
+                // Sử dụng hàm date($format, $timestamp) để chuyển đổi ngày thành định dạng Việt Nam (ngày/tháng/năm)
+                // Do hàm date() nhận vào là đối tượng thời gian, chúng ta cần sử dụng hàm strtotime() để chuyển đổi từ chuỗi có định dạng 'yyyy-mm-dd' trong MYSQL thành đối tượng ngày tháng
+                date('d/m/Y', strtotime($rowKhuyenMai['kh_tungay'])),
+                date('d/m/Y', strtotime($rowKhuyenMai['km_denngay']))
+            );
+          }
+          $dataKhuyenMai[] = array(
+            'km_ma' => $rowKhuyenMai['km_ma'],
+            'km_tomtat' => $km_tomtat,
+        );
+        }
+
+        ?>
+
         <!-- Block content -->
         <form action="" method="post" name="frmCreate" id="frmCreate">
-          <div class="form-group">
-            <label for="sp_ten">Tên sản phẩm</label>
-            <input type="text" class="form-control" id="sp_ten" name="sp_ten" aria-describedby="sp_tenHelp">
-            <small id="sp_tenHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
-            <label for="sp_gia">Giá</label>
-            <input type="text" class="form-control" id="sp_gia" name="sp_gia" aria-describedby="sp_giaHelp">
-            <small id="sp_giaHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
-            <label for="sp_giacu">Giá cũ</label>
-            <input type="text" class="form-control" id="sp_giacu" name="sp_giacu" aria-describedby="sp_giacuHelp">
-            <small id="sp_giacuHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
+        <div class="form-group">
+            <label for="sp_ten">Tên Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_ten" name="sp_ten" placeholder="Tên Sản phẩm" value="">
+        </div>
+        <div class="form-group">
+            <label for="sp_gia">Giá Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_gia" name="sp_gia" placeholder="Giá Sản phẩm" value="">
+        </div>
+        <div class="form-group">
+            <label for="sp_giacu">Giá cũ Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_giacu" name="sp_giacu" placeholder="Giá cũ Sản phẩm" value="">
+        </div>
+        <div class="form-group">
             <label for="sp_mota_ngan">Mô tả ngắn</label>
-            <textarea type="text" class="form-control" id="sp_mota_ngan" name="sp_mota_ngan" aria-describedby="sp_mota_nganHelp"></textarea>
-            
-          </div>
-          <div class="form-group">
+            <textarea class="form-control" id="sp_mota_ngan" name="sp_mota_ngan" ></textarea>
+        </div>
+        <div class="form-group">
             <label for="sp_mota_chitiet">Mô tả chi tiết</label>
-            <textarea type="text" class="form-control" id="sp_mota_chitiet" name="sp_mota_chitiet" aria-describedby="sp_mota_chitietHelp"></textarea>
-            
-          </div>
-          <div class="form-group">
+            <textarea class="form-control" id="sp_mota_chitiet" name="sp_mota_chitiet" ></textarea>
+        </div>
+        <div class="form-group">
             <label for="sp_ngaycapnhat">Ngày cập nhật</label>
-            <input type="datetime-local" class="form-control" id="sp_ngaycapnhat" name="sp_ngaycapnhat" aria-describedby="sp_ngaycapnhatHelp">
-            <small id="sp_ngaycapnhatHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
+            <input type="text" class="form-control" id="sp_ngaycapnhat" name="sp_ngaycapnhat" placeholder="Ngày cập nhật Sản phẩm" value="">
+        </div>
+        <div class="form-group">
             <label for="sp_soluong">Số lượng</label>
-            <input type="text" class="form-control" id="sp_soluong" name="sp_soluong" aria-describedby="sp_soluongHelp">
-            <small id="sp_soluongHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
-            <label for="lsp_ma">Mã loại sản phẩm</label>
-            <input type="text" class="form-control" id="lsp_ma" name="lsp_ma" aria-describedby="lsp_maHelp">
-            <small id="lsp_maHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
-            <label for="nsx_ma">Mã nhà sản xuất</label>
-            <input type="text" class="form-control" id="nsx_ma" name="nsx_ma" aria-describedby="nsx_maHelp">
-            <small id="nsx_maHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <div class="form-group">
-            <label for="km_ma">Mã khuyến mãi</label>
-            <input type="text" class="form-control" id="km_ma" name="km_ma" aria-describedby="km_maHelp">
-            <small id="km_maHelp" class="form-text text-muted">Nhập ít nhất 5 ký tự</small>
-          </div>
-          <button class="btn btn-primary" name="btnSave">Lưu dữ liệu</button>
+            <input type="text" class="form-control" id="sp_soluong" name="sp_soluong" placeholder="Số lượng Sản phẩm" value="">
+        </div>
+        <div class="form-group">
+            <label for="lsp_ma">Loại sản phẩm</label>
+            <select name="lsp_ma" id="lsp_ma" class="form-control">
+              <?php foreach($dataLoaiSanPham as $lsp):?>
+                <option value="<?= $lsp['lsp_ma']?>"><?= $lsp['lsp_ten']?></option>
+              <?php endforeach;?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="nsx_ma">Nhà sản xuất</label>
+            <select class="form-control" id="nsx_ma" name="nsx_ma">
+                <?php foreach ($dataNhaSanXuat as $nhasanxuat) : ?>
+                    <option value="<?= $nhasanxuat['nsx_ma'] ?>"><?= $nhasanxuat['nsx_ten'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        
+        <div class="form-group">
+            <label for="nsx_ma">Khuyến mãi</label>
+            <select class="form-control" id="km_ma" name="km_ma">
+                <option value="">Chọn loại khuyến mãi...</option>
+                <?php foreach ($dataKhuyenMai as $khuyenmai) : ?>
+                    <option value="<?= $khuyenmai['km_ma'] ?>"><?= $khuyenmai['km_tomtat'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <button class="btn btn-primary" name="btnSave">Lưu dữ liệu</button>
         </form>
 
         <?php
-          // Truy vấn database
-          // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
-          include_once(__DIR__ . '/../../../dbconnect.php');
-
-          // 2. Nếu người dùng có bấm nút "Lưu dữ liệu"
+        // 2. Nếu người dùng có bấm nút "Lưu dữ liệu"
           if(isset($_POST['btnSave'])){
             // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
-            $sp_ten = $_POST['sp_ten'];
-            $sp_mota = $_POST['sp_mota'];
+            $ten = $_POST['sp_ten'];
+            $gia = $_POST['sp_gia'];
+            $giacu = $_POST['sp_giacu'];
+            $motangan = $_POST['sp_mota_ngan'];
+            $motachitiet = $_POST['sp_mota_chitiet'];
+            $ngaycapnhat = $_POST['sp_ngaycapnhat'];
+            $soluong = $_POST['sp_soluong'];
+            $lsp_ma = $_POST['lsp_ma'];
+            $nsx_ma = $_POST['nsx_ma'];
+            $km_ma = (empty($_POST['km_ma']) ? 'NULL' : $_POST['km_ma']);
 
             // Kiểm tra ràng buộc dữ liệu (Validation)
             // Tạo biến lỗi để chứa thông báo lỗi
             $errors = [];
 
-            // Validate Tên sản phẩm
+            // Validate Tên  Sản phẩm
             // required
-            if(empty($sp_ten)){
-              $errors['sp_ten'][] = [
+            if(empty($ten)){
+              $errors['ten'][] = [
                 'rule' => 'required',
                 'rule_value' => true,
-                'value' => $sp_ten,
-                'msg' => 'Vui lòng nhập tên sản phẩm'
+                'value' => $ten,
+                'msg' => 'Vui lòng nhập tên  sản phẩm'
               ];
             }
             // minlength 3
-            if (!empty($sp_ten) && strlen($sp_ten) < 5) {
-              $errors['sp_ten'][] = [
+            if (!empty($ten) && strlen($ten) < 5) {
+              $errors['ten'][] = [
                 'rule' => 'minlength',
                 'rule_value' => 5,
-                'value' => $sp_ten,
-                'msg' => 'Tên sản phẩm phải có ít nhất 5 ký tự'
+                'value' => $ten,
+                'msg' => 'Tên  sản phẩm phải có ít nhất 5 ký tự'
               ];
             }
             // maxlength 50
-            if (!empty($sp_ten) && strlen($sp_ten) > 50) {
-              $errors['sp_ten'][] = [
+            if (!empty($ten) && strlen($ten) > 50) {
+              $errors['ten'][] = [
                 'rule' => 'maxlength',
                 'rule_value' => 50,
-                'value' => $sp_ten,
-                'msg' => 'Tên sản phẩm không được vượt quá 50 ký tự'
+                'value' => $ten,
+                'msg' => 'Tên  sản phẩm không được vượt quá 50 ký tự'
               ];
             }
 
-            // Validate Diễn giải
+            // Validate giá
             // required
-            if (empty($sp_mota)) {
-              $errors['sp_mota'][] = [
+            if (empty($gia)) {
+              $errors['gia'][] = [
                 'rule' => 'required',
                 'rule_value' => true,
-                'value' => $sp_mota,
-                'msg' => 'Vui lòng nhập mô tả sản phẩm'
+                'value' => $gia,
+                'msg' => 'Vui lòng nhập giá  sản phẩm'
               ];
-            }
-            // minlength 3
-            if (!empty($sp_mota) && strlen($sp_mota) < 5) {
-              $errors['sp_mota'][] = [
-                'rule' => 'minlength',
-                'rule_value' => 5,
-                'value' => $sp_mota,
-                'msg' => 'Mô tả sản phẩm phải có ít nhất 5 ký tự'
+            }            
+            // Validate giá cũ
+            // required
+            if (empty($giacu)) {
+              $errors['gia'][] = [
+                'rule' => 'required',
+                'rule_value' => true,
+                'value' => $giacu,
+                'msg' => 'Vui lòng nhập giá cũ sản phẩm'
               ];
-            }
-            // maxlength 255
-            if (!empty($sp_mota) && strlen($sp_mota) > 255) {
-              $errors['sp_mota'][] = [
-                'rule' => 'maxlength',
-                'rule_value' => 255,
-                'value' => $sp_mota,
-                'msg' => 'Mô tả sản phẩm không được vượt quá 255 ký tự'
-              ];
-            }
+            }            
           }
             ?>
 
@@ -178,32 +259,27 @@
             </div>
           <?php endif; ?>
 
-          <?php
-            // Nếu không có lỗi VALIDATE dữ liệu (tức là dữ liệu đã hợp lệ)
-            // Tiến hành thực thi câu lệnh SQL Query Database
-            // => giá trị của biến $errors là rỗng
-            if (
-              isset($_POST['btnSave'])  // Nếu người dùng có bấm nút "Lưu dữ liệu"
-              && (!isset($errors) || (empty($errors))) // Nếu biến $errors không tồn tại Hoặc giá trị của biến $errors rỗng
-            ) {
-              // VALIDATE dữ liệu đã hợp lệ
-              // Thực thi câu lệnh SQL QUERY
-              // Câu lệnh INSERT
-              $sql = "INSERT INTO `loaisanpham` (sp_ten, sp_mota) VALUES ('$sp_ten', '$sp_mota');";
+        <?php
+                    // 2. Nếu người dùng có bấm nút Đăng ký thì thực thi câu lệnh UPDATE
+          if (isset($_POST['btnSave'])) {
+            // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
+            
+            // Câu lệnh INSERT
+            $sql = <<<EOT
+            INSERT INTO sanpham (sp_ten, sp_gia, sp_giacu, sp_mota_ngan, sp_mota_chitiet, sp_ngaycapnhat, sp_soluong, lsp_ma, nsx_ma, km_ma) 
+            VALUES ('$ten', $gia, $giacu, '$motangan', '$motachitiet', '$ngaycapnhat', $soluong, $lsp_ma, $nsx_ma, $km_ma);
+EOT;
+            // var_dump($sql);die;
+            // Thực thi INSERT
+            mysqli_query($conn, $sql);
 
-              // Thực thi INSERT
-              mysqli_query($conn, $sql) or die("<b>Có lỗi khi thực thi câu lệnh SQL: </b>" . mysqli_error($conn) . "<br /><b>Câu lệnh vừa thực thi:</b></br>$sql");
+            // Đóng kết nối
+            mysqli_close($conn);
 
-              // Đóng kết nối
-              mysqli_close($conn);
-
-              // Sau khi cập nhật dữ liệu, tự động điều hướng về trang Danh sách
-              // Điều hướng bằng Javascript
-              echo '<script>location.href = "index.php";</script>';
-            }
-            ?>
-          
-          
+            // Sau khi cập nhật dữ liệu, tự động điều hướng về trang Danh sách
+            echo "<script>location.href = 'index.php';</script>";
+          }
+        ?>
 
         <!-- End block content -->
       </main>
@@ -222,49 +298,7 @@
 
   <script>
   $(document).ready(function() {
-    $("#frmCreate").validate({
-      rules: {
-        sp_ten: {
-          required: true,
-          minlength: 5,
-          maxlength: 50
-        },
-        sp_mota: {
-          required: true,
-          minlength: 5,
-          maxlength: 255
-        }
-      },
-      messages: {
-        sp_ten: {
-          required: "Vui lòng nhập tên sản phẩm",
-          minlength: "Tên sản phẩm phải có ít nhất 5 ký tự",
-          maxlength: "Tên sản phẩm không được vượt quá 50 ký tự"
-        },
-        sp_mota: {
-          required: "Vui lòng nhập mô tả cho sản phẩm",
-          minlength: "Mô tả cho sản phẩm phải có ít nhất 5 ký tự",
-          maxlength: "Mô tả cho sản phẩm không được vượt quá 255 ký tự"
-        },
-      },
-      errorElement: "em",
-      errorPlacement: function(error, element) {
-        // Thêm class `invalid-feedback` cho field đang có lỗi
-        error.addClass("invalid-feedback");
-        if (element.prop("type") === "checkbox") {
-          error.insertAfter(element.parent("label"));
-        } else {
-          error.insertAfter(element);
-        }
-      },
-      success: function(label, element) {},
-      highlight: function(element, errorClass, validClass) {
-        $(element).addClass("is-invalid").removeClass("is-valid");
-      },
-      unhighlight: function(element, errorClass, validClass) {
-        $(element).addClass("is-valid").removeClass("is-invalid");
-      }
-    });
+    
   });
 </script>
 
--- backend/functions/sanpham/edit.php
+++ backend/functions/sanpham/edit.php
@@ -22,11 +22,198 @@
 
       <main role="main" class="col-md-10 ml-sm-auto px-4 mb-2">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Sửa loại sản phẩm</h1>
+          <h1 class="h2">Sửa sản phẩm</h1>
         </div>
 
+        <?php
+        // Truy vấn database
+        // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+        include_once(__DIR__ . '/../../../dbconnect.php');
+
+        $sp_ma = $_GET['sp_ma'];
+
+        $sqlSelectSanPham = "select * from sanpham where sp_ma = $sp_ma";
+        $resultSanPham = mysqli_query($conn, $sqlSelectSanPham);
+        $rowSanPham = mysqli_fetch_array($resultSanPham, MYSQLI_ASSOC);
+
+
+        /* --- 
+        --- 2.Truy vấn dữ liệu Loại sản phẩm 
+        --- 
+        */
+        // Chuẩn bị câu truy vấn Loại sản phẩm
+        $sqlLoaiSanPham = "select * from loaisanpham";
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultLoaiSanPham = mysqli_query($conn, $sqlLoaiSanPham);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataLoaiSanPham = [];
+        while ($rowLoaiSanPham = mysqli_fetch_array($resultLoaiSanPham, MYSQLI_ASSOC)) {
+            $dataLoaiSanPham[] = array(
+                'lsp_ma' => $rowLoaiSanPham['lsp_ma'],
+                'lsp_ten' => $rowLoaiSanPham['lsp_ten'],
+                'lsp_mota' => $rowLoaiSanPham['lsp_mota'],
+            );
+        }
+        /* --- End Truy vấn dữ liệu Loại sản phẩm --- */
+        
+        /* --- 
+        --- 3. Truy vấn dữ liệu Nhà sản xuất 
+        --- 
+        */
+        // Chuẩn bị câu truy vấn Nhà sản xuất
+        $sqlNhaSanXuat = "select * from nhasanxuat";
+
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultNhaSanXuat = mysqli_query($conn, $sqlNhaSanXuat);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataNhaSanXuat = [];
+        while ($rowNhaSanXuat = mysqli_fetch_array($resultNhaSanXuat, MYSQLI_ASSOC)) {
+            $dataNhaSanXuat[] = array(
+                'nsx_ma' => $rowNhaSanXuat['nsx_ma'],
+                'nsx_ten' => $rowNhaSanXuat['nsx_ten'],
+            );
+        }
+        /* --- End Truy vấn dữ liệu Nhà sản xuất --- */
+
+        // Chuẩn bị câu truy vấn Khuyến mãi
+        $sqlKhuyenMai = "select * from `khuyenmai`";
+
+        // Thực thi câu truy vấn SQL để lấy về dữ liệu
+        $resultKhuyenMai = mysqli_query($conn, $sqlKhuyenMai);
+
+        // Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
+        // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
+        // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
+        $dataKhuyenMai = [];
+        while($rowKhuyenMai = mysqli_fetch_array($resultKhuyenMai, MYSQLI_ASSOC)){
+          $km_tomtat = '';
+          if(!empty($rowKhuyenMai['km_ten'])){
+            // Sử dụng hàm sprintf() để chuẩn bị mẫu câu với các giá trị truyền vào tương ứng từng vị trí placeholder
+            $km_tomtat = sprintf(
+                "Khuyến mãi %s, nội dung: %s, thời gian: %s-%s",
+                $rowKhuyenMai['km_ten'],
+                $rowKhuyenMai['kh_noidung'],
+                // Sử dụng hàm date($format, $timestamp) để chuyển đổi ngày thành định dạng Việt Nam (ngày/tháng/năm)
+                // Do hàm date() nhận vào là đối tượng thời gian, chúng ta cần sử dụng hàm strtotime() để chuyển đổi từ chuỗi có định dạng 'yyyy-mm-dd' trong MYSQL thành đối tượng ngày tháng
+                date('d/m/Y', strtotime($rowKhuyenMai['kh_tungay'])),
+                date('d/m/Y', strtotime($rowKhuyenMai['km_denngay']))
+            );
+          }
+          $dataKhuyenMai[] = array(
+            'km_ma' => $rowKhuyenMai['km_ma'],
+            'km_tomtat' => $km_tomtat,
+        );
+        }
+
+        ?>
+
         <!-- Block content -->
-            <?= $_GET['lsp_ma']?>
+        <form action="" method="post" name="frmCreate" id="frmCreate">
+        <div class="form-group">
+            <label for="sp_ten">Tên Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_ten" name="sp_ten" placeholder="Tên Sản phẩm" value="<?= $rowSanPham['sp_ten']?>">
+        </div>
+        <div class="form-group">
+            <label for="sp_gia">Giá Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_gia" name="sp_gia" placeholder="Giá Sản phẩm" value="<?= $rowSanPham['sp_gia']?>">
+        </div>
+        <div class="form-group">
+            <label for="sp_giacu">Giá cũ Sản phẩm</label>
+            <input type="text" class="form-control" id="sp_giacu" name="sp_giacu" placeholder="Giá cũ Sản phẩm" value="<?= $rowSanPham['sp_giacu']?>">
+        </div>
+        <div class="form-group">
+            <label for="sp_mota_ngan">Mô tả ngắn</label>
+            <textarea class="form-control" id="sp_mota_ngan" name="sp_mota_ngan" ><?= $rowSanPham['sp_mota_ngan']?></textarea>
+        </div>
+        <div class="form-group">
+            <label for="sp_mota_chitiet">Mô tả chi tiết</label>
+            <textarea class="form-control" id="sp_mota_chitiet" name="sp_mota_chitiet" ><?= $rowSanPham['sp_mota_chitiet']?></textarea>
+        </div>
+        <div class="form-group">
+            <label for="sp_ngaycapnhat">Ngày cập nhật</label>
+            <input type="text" class="form-control" id="sp_ngaycapnhat" name="sp_ngaycapnhat" placeholder="Ngày cập nhật Sản phẩm" value="<?= $rowSanPham['sp_ngaycapnhat']?>">
+        </div>
+        <div class="form-group">
+            <label for="sp_soluong">Số lượng</label>
+            <input type="text" class="form-control" id="sp_soluong" name="sp_soluong" placeholder="Số lượng Sản phẩm" value="<?= $rowSanPham['sp_soluong']?>">
+        </div>
+        <div class="form-group">
+            <label for="lsp_ma">Loại sản phẩm</label>
+            <select name="lsp_ma" id="lsp_ma" class="form-control">
+              <?php foreach($dataLoaiSanPham as $lsp):?>
+                <option value="<?= $lsp['lsp_ma']?>"><?= $lsp['lsp_ten']?></option>
+              <?php endforeach;?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="nsx_ma">Nhà sản xuất</label>
+            <select class="form-control" id="nsx_ma" name="nsx_ma">
+                <?php foreach ($dataNhaSanXuat as $nhasanxuat) : ?>
+                    <option value="<?= $nhasanxuat['nsx_ma'] ?>"><?= $nhasanxuat['nsx_ten'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        
+        <div class="form-group">
+            <label for="nsx_ma">Khuyến mãi</label>
+            <select class="form-control" id="km_ma" name="km_ma">
+                <option value="">Chọn loại khuyến mãi...</option>
+                <?php foreach ($dataKhuyenMai as $khuyenmai) : ?>
+                    <option value="<?= $khuyenmai['km_ma'] ?>"><?= $khuyenmai['km_tomtat'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <button class="btn btn-primary" name="btnSave">Lưu dữ liệu</button>
+        </form>
+
+        <?php
+          // 2. Nếu người dùng có bấm nút Đăng ký thì thực thi câu lệnh UPDATE
+          if (isset($_POST['btnSave'])) {
+            // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
+            $ten = $_POST['sp_ten'];
+            $gia = $_POST['sp_gia'];
+            $giacu = $_POST['sp_giacu'];
+            $motangan = $_POST['sp_mota_ngan'];
+            $motachitiet = $_POST['sp_mota_chitiet'];
+            $ngaycapnhat = $_POST['sp_ngaycapnhat'];
+            $soluong = $_POST['sp_soluong'];
+            $lsp_ma = $_POST['lsp_ma'];
+            $nsx_ma = $_POST['nsx_ma'];
+            $km_ma = (empty($_POST['km_ma']) ? 'NULL' : $_POST['km_ma']);
+            // Câu lệnh INSERT
+            $sql = <<<EOT
+            UPDATE sanpham
+            SET		
+              sp_ten='$ten',
+              sp_gia=$gia,
+              sp_giacu=$giacu,
+              sp_mota_ngan='$motangan',
+              sp_mota_chitiet='$motachitiet',
+              sp_ngaycapnhat='$ngaycapnhat',
+              sp_soluong=$soluong,
+              lsp_ma=$lsp_ma,
+              nsx_ma=$nsx_ma,
+              km_ma=$km_ma
+            WHERE sp_ma=$sp_ma;
+EOT;
+            // var_dump($sql);die;
+            // Thực thi INSERT
+            mysqli_query($conn, $sql);
+
+            // Đóng kết nối
+            mysqli_close($conn);
+
+            // Sau khi cập nhật dữ liệu, tự động điều hướng về trang Danh sách
+            echo "<script>location.href = 'index.php';</script>";
+          }
+        ?>
+
         <!-- End block content -->
       </main>
     </div>
@@ -41,6 +228,13 @@
 
   <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
   <!-- <script src="..."></script> -->
+
+  <script>
+  $(document).ready(function() {
+    
+  });
+</script>
+
 </body>
 
 </html>
